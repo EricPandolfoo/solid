e874862acc49eba1e9fa8571669ff31e59faddb7
Eric Pandolfo <eric.pandolfo@gmail.com>
2022-04-13 15:03:28 -0300

    teste

--- src/main/java/com/eric/solid/SolidApplication.java
+++ src/main/java/com/eric/solid/SolidApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SolidApplication {
 
 	//Texto teste
+	//Segundo texto
 	public static void main(String[] args) {
 		SpringApplication.run(SolidApplication.class, args);
 	}
