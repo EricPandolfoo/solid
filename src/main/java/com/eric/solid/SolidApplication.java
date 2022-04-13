a0e1e563e9eff2af39d5241743dc47c15336379f
Eric Pandolfo <eric.pandolfo@gmail.com>
2022-04-13 15:51:02 -0300

    revert test

--- src/main/java/com/eric/solid/SolidApplication.java
+++ src/main/java/com/eric/solid/SolidApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SolidApplication {
 
 	//Texto teste
-	//Segundo texto
 	public static void main(String[] args) {
 		SpringApplication.run(SolidApplication.class, args);
 	}
